385c674f64688bf631fd0139a25fbfa2d23eac13
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-07-14 20:54:25 -0600

    Update credentials.py

--- etl-pipeline/credentials/credentials.py
+++ etl-pipeline/credentials/credentials.py
@@ -1,9 +1,17 @@
+from kaggle.api.kaggle_api_extended import KaggleApi
+
 class load:
     """Storing all credentials"""
-    
-    data_set_name =
-    api = 
-    project_id = 
-    dataset_id = 
-    table_id = 
-    google_credentials = 
+    data_set_name = "berkayclik/fake-passenger-name-record-pnr-data-amadeus" # for testing purposes
+    #data_set_name = "cameronseamons/electronic-sales-sep2023-sep2024"
+    #csv = 'C:/Users/loydt/Documents/datasets/raw/Electronic_sales_Sep2023-Sep2024.csv'
+    csv = "C:/Users/loydt/Documents/datasets/raw/trips.csv"
+    api = KaggleApi()
+    project_id = "data-analysis-404209"
+    dataset_id = "sales_data"
+    table_id = "completed_orders"
+    google_credentials = "C:/Users/loydt/Documents/my_package/data-analysis-404209-0f170f6d3180.json"
+    postgresql_host = "localhost"
+    postgresql = "postgres"
+    postgresql_pass = ""
+    postgresql_port = ""
