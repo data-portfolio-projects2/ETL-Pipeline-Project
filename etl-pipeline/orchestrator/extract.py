b00dcf33aa7c6c5a9f376e46dfa61c83e6ceb301
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-07-14 21:01:25 -0600

    Delete extract.py

--- etl-pipeline/orchestrator/extract.py
+++
@@ -1,9 +0,0 @@
-class Run(metaclass=static):
-
-    @time
-    @memory
-    def extract():
-        download.authenticate_kaggle()
-        download.initiate_os()
-        download.download_data()
-
