c43f92ebd21793d9e4a102ef0bae271a1427eb5b
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-07-14 21:01:55 -0600

    Delete load.py

--- etl-pipeline/orchestrator/load.py
+++
@@ -1,9 +0,0 @@
-    @time
-    @memory 
-    def load():
-        df = load.completed_orders()
-        df = load.transform_price(df)
-        df = load.transform_dates(df)
-        df = load.create_sales(df)
-        load.create_table(df)
-
