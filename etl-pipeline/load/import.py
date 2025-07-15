c0922bc02e2ba64b33fb4c2ce707388609965eb3
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-07-14 20:58:17 -0600

    Delete import.py

--- etl-pipeline/load/import.py
+++
@@ -1,8 +0,0 @@
-from monitor import logging as log, static
-from google.cloud import bigquery
-from credentials import load
-from processed import processed
-from pandas_gbq import to_gbq
-from category import order
-import pandas as pd
-import os
